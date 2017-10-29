f1935736cae529b0cf6bdad57ac24215c73be2fc
UmbraFerreus <mzhukov31415dev@gmail.com>
2017-10-29 05:24:45 +0300

    Updates

--- src/modules/initialStates.js
+++
@@ -1,6 +0,0 @@
-export const cmpDataInit = {
-  componentsText: 'Placeholder',
-  width:          256,
-  height:         256,
-  modPrefix:      'CmpC_'+Date.now().toString(16).slice(4)+'_CmpC_'
-}
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import cmpData from './cmpData'
-
-const mainReducer = combineReducers({
-  cmpData
-});
-
-export default mainReducer
